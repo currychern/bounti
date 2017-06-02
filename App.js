58196c1523853e3068a504c76511275c0c40ce07
Curry Chern <curry.chern@gmail.com>
2017-06-02 16:47:01 -0500

    quick build of search page

--- App.js
+++ App.js
@@ -1,15 +1,92 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Dimensions, Button, Alert } from 'react-native';
+import { Entypo, Octicons, Ionicons, FontAwesome } from '@expo/vector-icons';
 
-export default class App extends React.Component {
+class SearchButton extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello, world!</Text>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <FontAwesome
+        style={{width: 40, height: 40, backgroundColor: '#ef7049', paddingLeft: 9, paddingTop: 7}}
+        name="search" size={25} color="#fdf1ed" onPress={onButtonPress} />
+    );
+  }
+}
+
+const onButtonPress = () => {
+  Alert.alert('Button has been pressed!');
+};
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={{flex: 1, flexDirection: 'column'}}>
+        <Header />
+        <BackgroundImage>
+          <View style={{flexDirection: 'row'}}>
+            <SearchBar />
+            <SearchButton />
+          </View>
+        </BackgroundImage>
+        <Footer />
       </View>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <View style={{width: null, height: 25, backgroundColor: '#00bd8a'}} />
+    );
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <View style={{width: null, height: 50, backgroundColor: '#00bd8a', flexDirection: 'row', justifyContent: 'center',}}>
+        <Entypo
+          style={{flex: 1, paddingLeft: 50, paddingTop: 5}}
+          name="bowl" size={35} color="white" onPress={onButtonPress} />
+        <Octicons
+          style={{flex: 1, paddingTop: 5}}
+          name="gift" size={40} color="white" onPress={onButtonPress} />
+        <Ionicons
+          style={{flex: 1, paddingTop: 5}}
+          name="md-more" size={40} color="white" onPress={onButtonPress} />
+      </View>
+    );
+  }
+}
+
+class BackgroundImage extends Component {
+  render() {
+    return (
+      <Image source={require('./img/sharing-tomatoes.jpg')}
+             style={styles.backgroundImage}>
+        <View style={styles.container}>
+          {this.props.children}
+        </View>
+      </Image>
+    );
+  }
+}
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+  }
+
+  render() {
+    return (
+      <TextInput
+        style={styles.search}
+        onChangeText={(text) => this.setState({text})}
+        placeholder='What are you looking for?'
+        placeholderTextColor = '#757575'
+        value={this.state.text}
+      />
     );
   }
 }
@@ -17,8 +94,26 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  backgroundImage: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover'
+  },
+  text: {
+    textAlign: 'center',
+    color: 'white',
+    backgroundColor: 'rgba(0,0,0,0)',
+    fontSize: 32
+  },
+  search: {
+    padding: 10,
+    height: 40,
+    width: Dimensions.get('window').width - 120,
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    fontSize: 15
   },
 });
