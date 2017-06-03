15034dc2a31661397cfabf7771fe7c9b4284b766
Kristin Faner <kfaner@gmail.com>
2017-06-03 14:42:29 -0500

    broke out footer and header components from app.js

---
+++ App/Components/Footer.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { View, Alert } from 'react-native';
+import { Entypo, Octicons, Ionicons } from '@expo/vector-icons';
+
+const onButtonPress = () => {
+  Alert.alert('Button has been pressed!');
+};
+
+class Footer extends Component {
+  render() {
+    return (
+      <View style={{width: null, height: 50, backgroundColor: '#00bd8a', flexDirection: 'row', justifyContent: 'center',}}>
+        <Entypo
+          style={{flex: 1, paddingLeft: 50, paddingTop: 5}}
+          name="bowl" size={35} color="white" onPress={onButtonPress} />
+        <Octicons
+          style={{flex: 1, paddingTop: 5}}
+          name="gift" size={40} color="white" onPress={onButtonPress} />
+        <Ionicons
+          style={{flex: 1, paddingTop: 5}}
+          name="md-more" size={40} color="white" onPress={onButtonPress} />
+      </View>
+    );
+  }
+}
+
+module.exports = Footer;
