a1b82d4ac0248321717b8c2cdc24aa1679dfb9e9
Curry Chern <curry.chern@gmail.com>
2017-06-04 13:55:36 -0500

    added Item view

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Text, View, Image, Button, Alert } from 'react-native';
 import Expo from 'expo';
 import HomeScreen from './App/Components/Routes/HomeScreen';
 import List from './App/Components/Routes/List';
+import Item from './App/Components/Routes/Item';
 
 import {
     createRouter,
@@ -13,6 +14,7 @@ import {
 const Router = createRouter(() => ({
     home: () => HomeScreen,
     list: () => List,
+    item: () => Item,
 }));
 
 // const onButtonPress = () => {
